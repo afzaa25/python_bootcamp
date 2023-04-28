4ab82211e1ea1b8a91e7c17a808eb9dcdca69303
Afzaa Atcha <afzaaatcha25@gmail.com>
2023-04-28 19:32:37 +0100

    day4_completed

--- day_4/lists.py
+++ day_4/lists.py
@@ -1,17 +1,24 @@
-# lists are a type of data structure
+# # lists are a type of data structure
 
-fruits = ["apple", "pear", "oranges"]
-print(fruits)
-#add something to list
-fruits.append("cherry")
-print(fruits)
-print(fruits[3]) #print list item at specific index number
-print(fruits[-1]) # prints last item in the list
+# fruits = ["apple", "pear", "oranges"]
+# print(fruits)
+# #add something to list
+# fruits.append("cherry")
+# print(fruits)
+# print(fruits[3]) #print list item at specific index number
+# print(fruits[-1]) # prints last item in the list
 
-fruits[2] = "banana" # this will replace list item at specific index
-print(fruits)
+# fruits[2] = "banana" # this will replace list item at specific index
+# print(fruits)
 
-fruits.insert(4, "strawberries") #insert at
-print(fruits)
+# fruits.insert(4, "strawberries") #insert at
+# print(fruits)
+# fruits.extend(['pears', 'grapes', 'watermelon', 'tangerines', 'mangoes'])
 
-#if you 
+# print(fruits)
+# #if you 
+
+#nested lists
+dirty_dozen = [['Spinach', 'Kale',  'Tomatoes', 'Celery', 'Potatoes'], ['Strawberries', 'Nectarines', 'Apples', 'Grapes', 'Peaches', 'Cherries', 'Pears', ]]
+
+print(dirty_dozen)
