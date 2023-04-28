59fedfebf1b1a08b737fdaea5543921e32fbcdc3
Afzaa Atcha <afzaaatcha25@gmail.com>
2023-04-28 11:19:45 +0100

    Initial Setup

---
+++ day_4/lists.py
@@ -0,0 +1,17 @@
+# lists are a type of data structure
+
+fruits = ["apple", "pear", "oranges"]
+print(fruits)
+#add something to list
+fruits.append("cherry")
+print(fruits)
+print(fruits[3]) #print list item at specific index number
+print(fruits[-1]) # prints last item in the list
+
+fruits[2] = "banana" # this will replace list item at specific index
+print(fruits)
+
+fruits.insert(4, "strawberries") #insert at
+print(fruits)
+
+#if you 
